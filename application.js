58c1fb075234864ae91ffa961b6924381515a533
Brennan Novak <brennan@reverseproductions.com>
2012-05-19 14:22:35 -0700

    added jquery-ui and started adding application

---
+++ application.js
@@ -0,0 +1,29 @@
+$(document).ready(function()
+{
+
+	// Draggable Materials
+	$('.draggable_material').draggable({ opacity: 0.6, helper: 'clone', revert: 'invalid' });
+
+
+	// Target Drop Spot
+	$('#added_materials').droppable(
+	{
+		disabled	: false,
+		addClasses	: false,
+		activeClass : 'occurrence_participants_hover',
+		hoverClass	: 'occurrence_participants_active',
+		drop 		: function(event, ui)
+		{
+			var material = ui.draggable.html();
+	
+			console.log(material);
+	
+	
+			$(this).append(material);
+	
+		}
+		
+	});
+	
+		
+});
